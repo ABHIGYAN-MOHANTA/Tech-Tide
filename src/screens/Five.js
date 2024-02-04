211483cb6a7db2cf391108e2dbf3f2196f13945f
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2024-02-04 15:59:37 +0530

    implemented back button

--- src/screens/Five.js
+++ src/screens/Five.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, BackHandler, Button } from "react-native";
 import WebView from "react-native-webview";
 
 const Five = () => {
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const webViewRef = React.useRef(null);
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      if (canGoBack) {
+        webViewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
+  const handleNavigationStateChange = (navState) => {
+    setCanGoBack(navState.canGoBack);
+  };
+
+  const goBack = () => {
+    if (canGoBack) {
+      webViewRef.current.goBack();
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, width: "100%" }}>
       <WebView
+        ref={webViewRef}
         style={{ flex: 1, width: "100%" }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         source={{
           uri: "https://paperswithcode.com/sota",
         }}
+        onNavigationStateChange={handleNavigationStateChange}
       />
+      {canGoBack && <Button title="Go Back" onPress={goBack} />}
     </SafeAreaView>
   );
 };
--- src/screens/SystemDesign.js
+++ src/screens/SystemDesign.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, BackHandler, Button } from "react-native";
 import WebView from "react-native-webview";
 
 const SystemDesign = () => {
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleBackPress
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  const handleBackPress = () => {
+    if (canGoBack) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
+
+  const webViewRef = React.useRef(null);
+
+  const handleNavigationStateChange = (navState) => {
+    setCanGoBack(navState.canGoBack);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, width: "100%" }}>
       <WebView
+        ref={webViewRef}
         style={{ flex: 1, width: "100%" }}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         source={{
           uri: "https://newsletter.systemdesign.one/?r=n49ky",
         }}
+        onNavigationStateChange={handleNavigationStateChange}
       />
+      {canGoBack && <Button title="Go Back" onPress={handleBackPress} />}
     </SafeAreaView>
   );
 };
